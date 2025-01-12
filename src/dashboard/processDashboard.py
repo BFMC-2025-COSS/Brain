97b4e764249a2419e47d0e928eb499f5e5a3c7ad
jwoon0906 <jwoon0906@naver.com>
2025-01-13 03:22:36 +0900

    picamera return to opencv2

--- src/dashboard/processDashboard.py
+++ src/dashboard/processDashboard.py
@@ -67,28 +67,25 @@ class processDashboard(WorkerProcess):
 
         self.sessionActive = False
         
-        self.app = Flask(__name__) # Flask 서버 초기화
-        self.socketio = SocketIO(self.app, cors_allowed_origins="*", async_mode='eventlet') # WebSocket 초기화
+        self.app = Flask(__name__)
+        self.socketio = SocketIO(self.app, cors_allowed_origins="*", async_mode='eventlet')
         CORS(self.app, supports_credentials=True)
 
-        self.getNamesAndVals() #allMessages.py에서 정의된 메시지 읽어와
-        # Remove the mainCamera and Semaphores message sender(대시보드에서 필요 없어서)
+        self.getNamesAndVals()
+        # Remove the mainCamera and Semaphores message sender
         self.messagesAndVals.pop("mainCamera")
         self.messagesAndVals.pop("Semaphores")
 
-        self.subscribe() #만약 owner가 Dashboard가 아니면 구독하고, Dashboard면 송신
+        self.subscribe()
 
         # Define WebSocket event handlers
         self.socketio.on_event('message', self.handleMessage)
-        #프론트엔드에서 데이터를 요청하거나 명령을 보낼 때 사용,백엔드가 요청을 처리하고 결과를 반환.
         self.socketio.on_event('save', self.handleSaveTableState)
-        #프론트엔드에서 대시보드 상태를 저장.
         self.socketio.on_event('load', self.handleLoadTableState)
-        #프론트엔드에서 저장된 상태를 불러와 대시보드에 적용.
 
         # Setting up a background task to automatically send the information to the host
-        self.sendContinuousHardwareData()# 하드웨어 상태 측정
-        self.socketio.start_background_task(self.sendContinuousMessages)# WebSocket으로 데이터 전송
+        self.sendContinuousHardwareData()
+        self.socketio.start_background_task(self.sendContinuousMessages)
         super(processDashboard, self).__init__(self.queueList)
 
     # ===================================== STOP ==========================================
@@ -110,16 +107,16 @@ class processDashboard(WorkerProcess):
         """Subscribe function. In this function we make all the required subscribe to process gateway"""
         
         for name, enum in self.messagesAndVals.items():
-            if enum["owner"] != "Dashboard": #만약 owner가 Dashboard가 아니면 구독하고(사용자에게 표시해야해서)
+            if enum["owner"] != "Dashboard":
                 subscriber = messageHandlerSubscriber(self.queueList, enum["enum"], "lastOnly", True)
                 self.messages[name] = {"obj": subscriber}
-            else: # Dashboard면 processGateway 송신(사용자가 대시보드에서 명령을 내려서)
+            else:
                 sender = messageHandlerSender(self.queueList,enum["enum"])
                 self.sendMessages[str(name)] = {"obj": sender}
         subscriber = messageHandlerSubscriber(self.queueList, Semaphores, "fifo", True) # we need it as a fifo so we can see all the semaphores status
         self.messages["Semaphores"] = {"obj": subscriber}
                 
-    def getNamesAndVals(self): #allMessages.py에서 정의된 메시지 읽어와
+    def getNamesAndVals(self):
         classes = inspect.getmembers(allMessages, inspect.isclass)
         for name, cls in classes:
             if name != "Enum" and issubclass(cls, Enum):
@@ -137,7 +134,6 @@ class processDashboard(WorkerProcess):
             if not self.sessionActive:
                 self.sessionActive = True
                 self.socketio.emit('session_access', {'data': True}, room=socketId)
-                # WebSocket을 사용해 대시보드에 실시간 데이터를 전송
             else:
                 self.socketio.emit('session_access', {'data': False}, room=socketId)
         elif dataName == "SessionEnd":
@@ -151,14 +147,12 @@ class processDashboard(WorkerProcess):
         if self.debugging:
             self.logger.info("Received message: " + data)
         dataDict = json.loads(data)
-        #with open('/app/Brain/src/utils/table_state.json', 'w') as json_file: # change me(path) 
-        with open('/app/Brain/src/utils/table_state.json', 'w') as json_file: # change me(path) 
+        with open('/home/pi/Brain/src/utils/table_state.json', 'w') as json_file: # change me(path) 
             json.dump(dataDict, json_file, indent=4)  
 
     def handleLoadTableState(self, data):
-        #file_path = '/home/pi/Brain/src/utils/table_state.json' # change me(path)
-        file_path = '/app/Brain/src/utils/table_state.json' # change me(path)
-
+        file_path = '/home/pi/Brain/src/utils/table_state.json' # change me(path)
+        
         try:
             with open(file_path, 'r') as json_file:
                 dataDict = json.load(json_file) 
@@ -167,32 +161,14 @@ class processDashboard(WorkerProcess):
             emit('response', {'error': 'File not found. Please save the table state first.'})
         except json.JSONDecodeError:
             emit('response', {'error': 'Failed to parse JSON data from the file.'})
-    def get_cpu_temperature(self):
-        thermal_path = "/sys/class/thermal/thermal_zone1/temp"
-        try:
-            with open(thermal_path, "r") as file:
-                temp_millidegree = int(file.read().strip())
-                return round(temp_millidegree / 1000.0)
-        except FileNotFoundError:
-            raise RuntimeError("CPU thermal sensor not found!")
-        except Exception as e:
-            raise RuntimeError(f"Failed to read CPU temperature: {e}")
 
-
-    def sendContinuousHardwareData(self):# 하드웨어 상태 측정
+    def sendContinuousHardwareData(self):   
         self.memoryUsage = psutil.virtual_memory().percent
         self.cpuCoreUsage = psutil.cpu_percent(interval=1, percpu=True)
-        #self.cpuTemperature = round(psutil.sensors_temperatures()['CPU-therm'][0].current)
-        self.cpuTemperature = self.get_cpu_temperature()
+        self.cpuTemperature = round(psutil.sensors_temperatures()['cpu_thermal'][0].current)
         threading.Timer(1, self.sendContinuousHardwareData).start()
 
-    # def sendContinuousHardwareData(self):# 하드웨어 상태 측정
-    #     self.memoryUsage = psutil.virtual_memory().percent
-    #     self.cpuCoreUsage = psutil.cpu_percent(interval=1, percpu=True)
-    #     self.cpuTemperature = round(psutil.sensors_temperatures()['cpu_thermal'][0].current)
-    #     threading.Timer(1, self.sendContinuousHardwareData).start()
-
-    def sendContinuousMessages(self):# WebSocket으로 데이터 전송
+    def sendContinuousMessages(self):
         counter = 1   
         socketSleep = 0.025
         sendTime = 1 
